a68c60f28da0a2aa9815d1ee5875a4db9a90a4d9
Sindhu C <sindhuc@mediguru.io>
2025-01-26 23:10:21 +0530

    addedfiles

--- main.tf
+++ main.tf
@@ -1,38 +1,17 @@
-# Data source to check if the Organizational Unit already exists
-data "aws_organizations_organizational_unit" "dev" {
-  parent_id = "r-1p5x"
-  name      = "Dev"
-}
-
-# Conditionally create the "Dev" OU if it does not exist
+# Create the "Dev" Organizational Unit
 resource "aws_organizations_organizational_unit" "dev" {
-  count     = length(data.aws_organizations_organizational_unit.dev.id) == 0 ? 1 : 0
   parent_id = "r-1p5x"
   name      = "Dev"
 }
 
-# Data source to check if the Organizational Unit already exists
-data "aws_organizations_organizational_unit" "prod" {
-  parent_id = "r-1p5x"
-  name      = "Prod"
-}
-
-# Conditionally create the "Prod" OU if it does not exist
+# Create the "Prod" Organizational Unit
 resource "aws_organizations_organizational_unit" "prod" {
-  count     = length(data.aws_organizations_organizational_unit.prod.id) == 0 ? 1 : 0
   parent_id = "r-1p5x"
   name      = "Prod"
 }
 
-# Data source to check if the Organizational Unit already exists
-data "aws_organizations_organizational_unit" "security" {
-  parent_id = "r-1p5x"
-  name      = "Security"
-}
-
-# Conditionally create the "Security" OU if it does not exist
+# Create the "Security" Organizational Unit
 resource "aws_organizations_organizational_unit" "security" {
-  count     = length(data.aws_organizations_organizational_unit.security.id) == 0 ? 1 : 0
   parent_id = "r-1p5x"
   name      = "Security"
 }
