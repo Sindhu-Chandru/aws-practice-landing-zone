1f1d0d914d4a7a7e8458d10fb3b06d21748cce97
Sindhu C <sindhuc@mediguru.io>
2025-01-26 22:14:28 +0530

    added aws-id

--- main.tf
+++ main.tf
@@ -5,17 +5,17 @@ resource "aws_organizations_organization" "main" {
 
 # Create Organizational Units (OUs)
 resource "aws_organizations_organizational_unit" "dev" {
-  parent_id = aws_organizations_organization.main.id
+  parent_id = "r-1p5x"
   name      = "Dev"
 }
 
 resource "aws_organizations_organizational_unit" "prod" {
-  parent_id = aws_organizations_organization.main.id
+  parent_id = "r-1p5x"
   name      = "Prod"
 }
 
 resource "aws_organizations_organizational_unit" "security" {
-  parent_id = aws_organizations_organization.main.id
+  parent_id = "r-1p5x"
   name      = "Security"
 }
 
