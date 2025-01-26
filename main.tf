4df8b1504ef414457d9fc65aed75ccc6dc509aa1
Sindhu C <sindhuc@mediguru.io>
2025-01-26 23:08:33 +0530

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,38 @@
-provider "aws" {
-  region = "us-east-1"  # Set your region
-}
-
-# Check if the Organizational Units already exist
-data "aws_organizations_organizational_units" "existing_ous" {
-  filter {
-    name = "Dev"
-  }
-}
-
-data "aws_organizations_organizational_units" "existing_prod" {
-  filter {
-    name = "Prod"
-  }
-}
-
-data "aws_organizations_organizational_units" "existing_security" {
-  filter {
-    name = "Security"
-  }
-}
-
-# Create Organizational Unit for Dev if it doesn't already exist
-resource "aws_organizations_organizational_unit" "dev" {
-  count     = length(data.aws_organizations_organizational_units.existing_ous.ids) == 0 ? 1 : 0
+# Data source to check if the Organizational Unit already exists
+data "aws_organizations_organizational_unit" "dev" {
   parent_id = "r-1p5x"
   name      = "Dev"
 }
 
-# Create Organizational Unit for Prod if it doesn't already exist
-resource "aws_organizations_organizational_unit" "prod" {
-  count     = length(data.aws_organizations_organizational_units.existing_prod.ids) == 0 ? 1 : 0
+# Conditionally create the "Dev" OU if it does not exist
+resource "aws_organizations_organizational_unit" "dev" {
+  count     = length(data.aws_organizations_organizational_unit.dev.id) == 0 ? 1 : 0
+  parent_id = "r-1p5x"
+  name      = "Dev"
+}
+
+# Data source to check if the Organizational Unit already exists
+data "aws_organizations_organizational_unit" "prod" {
   parent_id = "r-1p5x"
   name      = "Prod"
 }
 
-# Create Organizational Unit for Security if it doesn't already exist
+# Conditionally create the "Prod" OU if it does not exist
+resource "aws_organizations_organizational_unit" "prod" {
+  count     = length(data.aws_organizations_organizational_unit.prod.id) == 0 ? 1 : 0
+  parent_id = "r-1p5x"
+  name      = "Prod"
+}
+
+# Data source to check if the Organizational Unit already exists
+data "aws_organizations_organizational_unit" "security" {
+  parent_id = "r-1p5x"
+  name      = "Security"
+}
+
+# Conditionally create the "Security" OU if it does not exist
 resource "aws_organizations_organizational_unit" "security" {
-  count     = length(data.aws_organizations_organizational_units.existing_security.ids) == 0 ? 1 : 0
+  count     = length(data.aws_organizations_organizational_unit.security.id) == 0 ? 1 : 0
   parent_id = "r-1p5x"
   name      = "Security"
 }
